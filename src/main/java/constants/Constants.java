e51c21c72de7cb2825c9d58877c0f4a37ca24f18
Kate Horokh <rinagorokh@gmail.com>
2019-11-03 23:58:55 +0200

    fixed constants

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,9 +1,9 @@
 package constants;
 
-public class Constants {
-    public static final String ENTER_NAME_OF_DEPARTMENT_NAME = "Enter name of department (Economy, Theatre, History, Math): ";
-    public static final String GLOBAL_SEARCH_BY = "Global search by: ";
+public interface Constants {
+    String ENTER_NAME_OF_DEPARTMENT_NAME = "Enter name of department (Economy, Theatre, History, Math): ";
+    String GLOBAL_SEARCH_BY = "Global search by: ";
 
     //Error messages
-    public static final String CAN_NOT_FOUND_BY_NAME = "Global search by: ";
+    String CAN_NOT_FOUND_BY_NAME = "Global search by: ";
 }
